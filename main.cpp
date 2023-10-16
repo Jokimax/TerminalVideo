3ae9cb6a9eae5d549924c030be4d3267aa6d8eb1
Jokimax <jonaxthedreadlord@gmail.com>
2023-10-16 23:18:39 +0300

    prevent high cpu usage

--- main.cpp
+++ main.cpp
@@ -36,6 +36,7 @@ int main(int argc, char** argv){
                     endwin();
                     return 0;
                 }
+                std::this_thread::sleep_for(std::chrono::milliseconds(5));
             }
 
         }
